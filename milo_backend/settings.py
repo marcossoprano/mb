c95fa955cc6dbb8d0dcb07d46aee32ffa708728f
Lara <lvssb@ic.ufal.br>
2025-07-29 20:06:16 -0300

    Integração - Terceira tentativa de commit

--- milo_backend/settings.py
+++ milo_backend/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'usuarios',
     'produtos',
-    'corsheaders',
+    'corsheaders', #Anotação da Lara: LEMBRE DE USAR pip install django-cors-headers
     
     
 ]
