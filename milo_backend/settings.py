161c3451ad28041cce4ddf9856393d851b526b4b
marcosryan <marcosryan140@gmail.com>
2025-09-29 12:33:55 -0300

    Fix ALLOWED_HOSTS for Render deployment

--- milo_backend/settings.py
+++ milo_backend/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+e0#b!42f+3(ksel0&z%6i7!c=58l49xi-1(k(&zze**%e5z3='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mb-rhj2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
